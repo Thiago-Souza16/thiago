5bafa89e83538cb0a8c14cbe5802d5dd8cb18b4b
Guilherme Ferreira <guilherme.ferreira-consultoria@ibopeconecta.com>
2020-01-22 15:28:28 -0300

    checkpoint

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes'); // importar routes.js
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes"); // importar routes.js
 
 const app = express();
 
-mongoose.connect('mongodb+srv://thiago:thiagosc@cluster0-ixgk9.gcp.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(
+  "mongodb+srv://thiago:thiagosc@cluster0-ixgk9.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true  
-})
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json()); //interação entre o express com json
 app.use(routes);
 
 //METODOS HTTP: GET, POST, DELETE E PUT
 
-// tipos de parametros no express: 
+// tipos de parametros no express:
 //Query Params: request.query (usado para filtros, ordenação, paginação...)
 //route params: request.params (identificar um recurso na alteração ou remoção)
 //body: request.body (Dados para criação ou alteração de algum registro)
 
 app.listen(3333); //caminho para abrir
+console.log("Running on port: 3333");
--- routes.js
+++ routes.js
@@ -1,25 +1,31 @@
-const{Router} = require('express') // importar const app do index
-const axios = require('axios')
+const { Router } = require("express"); // importar const app do index
+const axios = require("axios");
 
-const routes = Router()
+const routes = Router();
 
-//app.get('/', (request, response ) => {
-//    return response.json({menssage : "hello omnistack"}); //metodo de resposta ao front 
-//});
+routes.get("/", (request, response) => {
+  return response.json({ menssage: "hello omnistack" }); //metodo de resposta ao front
+});
 
-routes.post('/Devs', async (request, response ) => { // const app passa a ser routes
-    const {github_username} = request.body 
+routes.post("/Devs", async (request, response) => {
+  // const app passa a ser routes
+  const { github_username } = request.body;
 
-    const apiResponse = await axios.get(`https://api.github.com/user/${github_username}`)
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
 
-    let{ name = login, avatar, bio}
+  let name = apiResponse.data.login,
+    avatar = apiResponse.data.avatar_url,
+    bio = apiResponse.data.bio;
 
-    if(!name){
-        name = api.data.login;
-    }
-   
-    console.log(name, avatar, bio, github_username)
-    return response.json({menssage : "hello omnistack"}); //metodo de resposta ao front 
-}); 
+  //metodo de resposta ao front
+  return response.json({
+    name,
+    avatar,
+    bio,
+    github_username
+  });
+});
 
-module.exports = routes
+module.exports = routes;
